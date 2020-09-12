4dc3f53f54d80bd0c866dadb753d03a33c168a11
letsdelfino <41598982+letsdelfino@users.noreply.github.com>
2020-09-12 10:47:50 -0300

    primeiro commit de arquivos

---
+++ Compilador/Compilador.py
@@ -0,0 +1,8 @@
+#Dicionário de dados: No Python, os dicionários são coleções de itens desordenados com uma diferença bem grande quando comparados às 
+#outras coleções (lists, sets, tuples, etc): um elemento dentro de um dicionário possui uma chave atrelada a ele, uma espécie de 
+#identificador. Sendo assim, é muito utilizado quando queremos armazenar dados de forma organizada e que possuem identificação 
+#única (como acontece em bancos de dados). Basicamente são tabelas hash. 
+#Fonte: https://www.treinaweb.com.br/blog/principais-estruturas-de-dados-no-python/
+
+#Implementando o dicionário de dados
+
